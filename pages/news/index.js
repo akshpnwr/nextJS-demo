4bc78fe74ce6b86b52d5c4b1191e32cb856a3cbb
Akash Panwar <akashpanwar9976@gmail.com>
2022-10-25 18:51:45 +0530

    Linking pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,21 @@
 // our-domain.com/news
 
+import Link from 'next/link';
+
 const news = () => {
-  return <h1>This is the news page</h1>;
+  return (
+    <>
+      <h1>This is the news page</h1>
+      <ul>
+        <li>
+          <Link href='/news/nextjs-is-a-great-framework'>
+            NestJs is great framework
+          </Link>
+        </li>
+        <li>Someyhig else</li>
+      </ul>
+    </>
+  );
 };
 
 export default news;
